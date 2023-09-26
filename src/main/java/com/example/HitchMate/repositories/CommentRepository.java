ad65648b2629ed83d498cb2ca82c452a8ede0a17
Mikolaj <mikolaj.wituszynski@gmail.com>
2023-09-26 16:49:56 +0200

    update comment repo

--- src/main/java/com/example/HitchMate/repositories/CommentRepository.java
+++ src/main/java/com/example/HitchMate/repositories/CommentRepository.java
@@ -1,4 +1,8 @@
 package com.example.HitchMate.repositories;
 
-public interface CommentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository {
+
+
 }
