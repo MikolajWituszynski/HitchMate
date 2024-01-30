0223876fb711040b631aa11905839d484756d08a
Mikolaj <mikolaj.wituszynski@gmail.com>
2024-01-30 20:49:30 +0100

    authController added <needs work>

---
+++ src/main/java/com/example/HitchMate/controllers/AuthController.java
@@ -0,0 +1,35 @@
+package com.example.HitchMate.controllers;
+
+import com.example.HitchMate.repositories.RoleRepository;
+import com.example.HitchMate.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("api/auth")
+public class AuthController {
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+
+    }
+
+}
